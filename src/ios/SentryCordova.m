1a76a7f78035086fd476588304215fcfadc76a0f
Daniel Griesser <daniel.griesser.86@gmail.com>
2018-01-26 12:10:07 +0100

    release: 0.5.1

--- src/ios/SentryCordova.m
+++ src/ios/SentryCordova.m
@@ -2,7 +2,7 @@
 #import <Cordova/CDVAvailability.h>
 @import Sentry;
 
-NSString *const SentryCordovaVersionString = @"0.5.0";
+NSString *const SentryCordovaVersionString = @"0.5.1";
 NSString *const SentryCordovaSdkName = @"sentry-cordova";
 
 @implementation SentryCordova
