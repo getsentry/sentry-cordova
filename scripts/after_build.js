ff7bdd66cf0bbcd4c94a0cfba6e5b6ae37c03b17
HazA <daniel.griesser.86@gmail.com>
2018-05-07 10:16:48 +0200

    fix: Iterate over all build paths

--- scripts/after_build.js
+++ scripts/after_build.js
@@ -21,32 +21,6 @@ module.exports = function(ctx) {
       .digest('hex');
   }
 
-  // TODO iterate through all paths
-  const buildPath = ctx.opts.paths[0];
-  if (!fs.existsSync(buildPath)) {
-    console.error(`Sentry: build path does not exist ${buildPath}`);
-    console.error('This is not an Ionic project, please check out:');
-    console.error('https://docs.sentry.io/clients/javascript/sourcemaps/');
-    console.error(
-      'to find out how to correctly upload sourcemaps for your project.',
-    );
-    return;
-  }
-
-  const configFile = path.join('sentry.properties');
-
-  if (!fs.existsSync(configFile)) {
-    console.error('Sentry: sentry.properties does not exist in project root!`');
-    process.exit(1);
-    return;
-  }
-
-  const indexHtml = path.join(buildPath, 'index.html');
-  if (!fs.existsSync(configFile)) {
-    console.error('Sentry: index.html does not exist');
-    return;
-  }
-
   function walk(dir) {
     let results = [];
     let list = fs.readdirSync(dir);
@@ -62,73 +36,99 @@ module.exports = function(ctx) {
     return results;
   }
 
-  // Adding files to include
-  let includes = [];
-  walk(buildPath).forEach((file, index) => {
-    let f = path.basename(file);
-    let [name, ext1, ext2] = f.split('.');
-    // we only want js source files and the according sourcemaps (no css etc.)
-    if (ext1 === 'js' || (ext1 === 'js' && ext2 === 'map')) {
-      // ignore sw-toolbox file
-      if (name !== 'sw-toolbox') {
-        includes.push(file);
-      }
-    }
-  });
-
-  const algorithm = 'sha1';
-  let allHash = '';
-
-  includes.forEach(file => {
-    let shasum = crypto.createHash(algorithm);
-    shasum.update(fs.readFileSync(file));
-    allHash += shasum.digest('hex');
-  });
-
-  const shasum2 = crypto.createHash(algorithm);
-  shasum2.update(allHash);
-  const fileHash = shasum2.digest('hex');
-  const release = fileHash.slice(0, 20);
-
-  const regex = /<head>(?:[\s\S]*(<!-- sentry-cordova -->))?/g;
-  let contents = fs.readFileSync(indexHtml, {
-    encoding: 'utf-8',
-  });
-  const releaseSentry = `
-  <script>
-  (function(w){var i=w.SENTRY_RELEASE=w.SENTRY_RELEASE||{};i.id='${release}';})(this);
-  </script>
-  <!-- sentry-cordova -->`;
-  const replaceWith = `<head>${releaseSentry}`;
-  fs.writeFileSync(indexHtml, contents.replace(regex, replaceWith));
-
-  // This is allowed to fail because it's ionic specific
-  const projectRootIndexHtml = path.join(
-    ctx.opts.projectRoot,
-    'src',
-    'index.html',
-  );
-  if (fs.existsSync(projectRootIndexHtml)) {
-    contents = fs.readFileSync(projectRootIndexHtml, {
-      encoding: 'utf-8',
-    });
-    fs.writeFileSync(
-      projectRootIndexHtml,
-      contents.replace(regex, replaceWith),
-    );
+  const configFile = path.join('sentry.properties');
+  if (!fs.existsSync(configFile)) {
+    console.error('Sentry: sentry.properties does not exist in project root!`');
+    process.exit(1);
+    return;
   }
-
   const ignore = ['node_modules'];
-
   const sentryCli = new SentryCli(configFile);
 
-  return sentryCli.releases
-    .new(release)
-    .then(() =>
-      sentryCli.releases.uploadSourceMaps(release, {
-        include: includes,
-        ignore: ignore,
-      }),
-    )
-    .then(() => sentryCli.releases.finalize(release));
+  const allReleases = ctx.opts.paths.map(buildPath => {
+    if (!fs.existsSync(buildPath)) {
+      console.error(`Sentry: build path does not exist ${buildPath}`);
+      console.error('This is not an Ionic project, please check out:');
+      console.error('https://docs.sentry.io/clients/javascript/sourcemaps/');
+      console.error(
+        'to find out how to correctly upload sourcemaps for your project.',
+      );
+      return;
+    }
+
+    const indexHtml = path.join(buildPath, 'index.html');
+    if (!fs.existsSync(configFile)) {
+      console.error('Sentry: index.html does not exist');
+      return;
+    }
+
+    // Adding files to include
+    let includes = [];
+    walk(buildPath).forEach((file, index) => {
+      let f = path.basename(file);
+      let [name, ext1, ext2] = f.split('.');
+      // we only want js source files and the according sourcemaps (no css etc.)
+      if (ext1 === 'js' || (ext1 === 'js' && ext2 === 'map')) {
+        // ignore sw-toolbox file
+        if (name !== 'sw-toolbox') {
+          includes.push(file);
+        }
+      }
+    });
+
+    const algorithm = 'sha1';
+    let allHash = '';
+
+    includes.forEach(file => {
+      let shasum = crypto.createHash(algorithm);
+      shasum.update(fs.readFileSync(file));
+      allHash += shasum.digest('hex');
+    });
+
+    const shasum2 = crypto.createHash(algorithm);
+    shasum2.update(allHash);
+    const fileHash = shasum2.digest('hex');
+    const release = fileHash.slice(0, 20);
+
+    const regex = /<head>(?:[\s\S]*(<!-- sentry-cordova -->))?/g;
+    let contents = fs.readFileSync(indexHtml, {
+      encoding: 'utf-8',
+    });
+    const releaseSentry = `
+    <script>
+    (function(w){var i=w.SENTRY_RELEASE=w.SENTRY_RELEASE||{};i.id='${release}';})(this);
+    </script>
+    <!-- sentry-cordova -->`;
+    const replaceWith = `<head>${releaseSentry}`;
+    fs.writeFileSync(indexHtml, contents.replace(regex, replaceWith));
+
+    // This is allowed to fail because it's ionic specific
+    const projectRootIndexHtml = path.join(
+      ctx.opts.projectRoot,
+      'src',
+      'index.html',
+    );
+    if (fs.existsSync(projectRootIndexHtml)) {
+      contents = fs.readFileSync(projectRootIndexHtml, {
+        encoding: 'utf-8',
+      });
+      fs.writeFileSync(
+        projectRootIndexHtml,
+        contents.replace(regex, replaceWith),
+      );
+    }
+
+    console.log(`Uploading assets release: '${release}' path: ${buildPath}`);
+    return sentryCli.releases
+      .new(release)
+      .then(() =>
+        sentryCli.releases.uploadSourceMaps(release, {
+          include: includes,
+          ignore: ignore,
+        }),
+      )
+      .then(() => sentryCli.releases.finalize(release));
+  });
+
+  return Promise.all(allReleases);
 };
