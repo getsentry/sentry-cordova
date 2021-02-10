152e70584dabefef7924c81a1b37d4caba8ba13e
jennmueng <ldrwarlock@gmail.com>
2021-02-10 17:30:55 +0700

    feat: Add ionic global error handler wrapper method

--- src/js/Ionic/SentryIonicErrorHandler.ts
+++ src/js/Ionic/SentryIonicErrorHandler.ts
@@ -1,10 +1,18 @@
-class SentryIonicErrorHandler extends IonicErrorHandler {
-  handleError(error) {
-    super.handleError(error);
-    try {
-      captureException(error.originalError || error);
-    } catch (e) {
-      console.error(e);
+import { captureException } from '@sentry/core';
+
+/**
+ * Wrap the ionic error handler with this method so Sentry catches unhandled errors on ionic.
+ * See the documentation for more details.
+ */
+const withSentryIonicErrorHandler = <C extends new (...args: any[]) => any>(IonicErrorHandler: C): C => {
+  class SentryIonicErrorHandler extends IonicErrorHandler {
+    handleError(error: any) {
+      super.handleError(error);
+      captureException(error.originalError ?? error);
     }
   }
-}
+
+  return SentryIonicErrorHandler;
+};
+
+export { withSentryIonicErrorHandler };
