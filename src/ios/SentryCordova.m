553556d72c2b32caf3c43b8909e95ad877b2b46c
jennmueng <ldrwarlock@gmail.com>
2021-02-14 01:30:52 +0700

    wip

--- src/ios/SentryCordova.m
+++ src/ios/SentryCordova.m
@@ -11,22 +11,31 @@ NSString *const SentryCordovaSdkName = @"sentry-cordova";
     NSLog(@"Sentry Cordova Plugin initialized");
 }
 
-- (void)install:(CDVInvokedUrlCommand *)command {
+- (void)getPlatform:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ios"];;
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)startWithOptions:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        NSString *dsn = [command.arguments objectAtIndex:0];
-        NSError *error = nil;
-        SentryClient *client = [[SentryClient alloc] initWithDsn:dsn didFailWithError:&error];
-        SentryClient.sharedClient = client;
-        [SentryClient.sharedClient startCrashHandlerWithError:&error];
-        client.beforeSerializeEvent = ^(SentryEvent * _Nonnull event) {
-            [self setReleaseVersionDist:event];
+        NSDictionary *options = [command.arguments objectAtIndex:0];
+
+        SentryBeforeSendEventCallback beforeSend = ^SentryEvent*(SentryEvent *event) {
+          [self setReleaseVersionDist:event];
+          return event;
         };
+        [options setValue:beforeSend forKey:@"beforeSend"];
+
+        NSError *error = nil;
+
+        SentryOptions *sentryOptions = [[SentryOptions alloc] initWithDict:options didFailWithError:&error];
+
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
         if (error != nil) {
             NSLog(@"%@", error);
             result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
         }
-        [SentryClient.sharedClient enableAutomaticBreadcrumbTracking];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     }];
 }
@@ -47,81 +56,106 @@ NSString *const SentryCordovaSdkName = @"sentry-cordova";
                   @"integrations": @[@"sentry-cocoa"]};
 }
 
-- (void)sendEvent:(CDVInvokedUrlCommand *)command {
+- (void)captureEnvelope:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        NSDictionary *jsonEvent = [command.arguments objectAtIndex:0];
-        if ([NSJSONSerialization isValidJSONObject:jsonEvent]) {
-            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonEvent
-                                                               options:0
-                                                                 error:nil];
+        NSDictionary *envelopeDict = [command.arguments objectAtIndex:0];
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
 
-            SentryEvent *sentryEvent = [[SentryEvent alloc] initWithJSON:jsonData];
-            if (SentryClient.sharedClient != nil) {
-                [SentryClient.sharedClient sendEvent:sentryEvent withCompletionHandler:^(NSError * _Nullable error) {
-                    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
-                    if (error != nil) {
-                        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
-                    }
-                    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-                }];
+        if ([NSJSONSerialization isValidJSONObject:envelopeDict]) {
+        SentrySdkInfo *sdkInfo = [[SentrySdkInfo alloc] initWithDict:envelopeDict[@"header"]];
+        SentryId *eventId = [[SentryId alloc] initWithUUIDString:envelopeDict[@"header"][@"event_id"]];
+        SentryEnvelopeHeader *envelopeHeader = [[SentryEnvelopeHeader alloc] initWithId:eventId andSdkInfo:sdkInfo];
+
+        NSError *error;
+        NSData *envelopeItemData = [NSJSONSerialization dataWithJSONObject:envelopeDict[@"payload"] options:0 error:&error];
+        if (nil != error) {
+          result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
+        } else {
+            NSString *itemType = envelopeDict[@"payload"][@"type"];
+            if (itemType == nil) {
+                // Default to event type.
+                itemType = @"event";
             }
-        }
+
+            SentryEnvelopeItemHeader *envelopeItemHeader = [[SentryEnvelopeItemHeader alloc] initWithType:itemType length:envelopeItemData.length];
+            SentryEnvelopeItem *envelopeItem = [[SentryEnvelopeItem alloc] initWithHeader:envelopeItemHeader data:envelopeItemData];
+
+            SentryEnvelope *envelope = [[SentryEnvelope alloc] initWithHeader:envelopeHeader singleItem:envelopeItem];
+
+            #if DEBUG
+                [[SentrySDK currentHub] captureEnvelope:envelope];
+            #else
+                if ([envelopeDict[@"payload"][@"level"] isEqualToString:@"fatal"]) {
+                    // Storing to disk happens asynchronously with captureEnvelope
+                    // We need to make sure the event is written to disk before resolving the promise.
+                    // This could be replaced by SentrySDK.flush() when available.
+                    [[[SentrySDK currentHub] getClient] storeEnvelope:envelope];
+                } else {
+                    [[SentrySDK currentHub] captureEnvelope:envelope];
+                }
+            #endif
+          }
+      } else {
+          result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
+      }
+
+      [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     }];
 }
 
 - (void)addBreadcrumb:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        NSDictionary *jsonBreadcrumb = [command.arguments objectAtIndex:0];
-        if ([jsonBreadcrumb isKindOfClass:NSDictionary.class]) {
-          SentryBreadcrumb *breadcrumb = [SentryJavaScriptBridgeHelper createSentryBreadcrumbFromJavaScriptBreadcrumb:jsonBreadcrumb];
-          [SentryClient.sharedClient.breadcrumbs addBreadcrumb:breadcrumb];
-          CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:[breadcrumb serialize]];
-          [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-        }
+        // NSDictionary *jsonBreadcrumb = [command.arguments objectAtIndex:0];
+        // if ([jsonBreadcrumb isKindOfClass:NSDictionary.class]) {
+        //   SentryBreadcrumb *breadcrumb = [SentryJavaScriptBridgeHelper createSentryBreadcrumbFromJavaScriptBreadcrumb:jsonBreadcrumb];
+        //   [SentryClient.sharedClient.breadcrumbs addBreadcrumb:breadcrumb];
+        //   CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:[breadcrumb serialize]];
+        //   [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        // }
     }];
 }
 
 
 - (void)setUserContext:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        id arg = [command.arguments objectAtIndex:0];
-        if ([arg isKindOfClass:NSDictionary.class]) {
-            SentryClient.sharedClient.user = [SentryJavaScriptBridgeHelper createSentryUserFromJavaScriptUser:arg];
-        }
+        // id arg = [command.arguments objectAtIndex:0];
+        // if ([arg isKindOfClass:NSDictionary.class]) {
+        //     SentryClient.sharedClient.user = [SentryJavaScriptBridgeHelper createSentryUserFromJavaScriptUser:arg];
+        // }
     }];
 }
 
 - (void)setTagsContext:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        if (SentryClient.sharedClient && SentryClient.sharedClient.tags) {
-            id dict = [command.arguments objectAtIndex:0];
-            if ([dict isKindOfClass:NSDictionary.class]) {
-                NSMutableDictionary *newDict = [NSMutableDictionary new];
-                [newDict addEntriesFromDictionary:SentryClient.sharedClient.tags];
-                [newDict addEntriesFromDictionary:dict];
-                SentryClient.sharedClient.tags = newDict;
-            }
-        }
+        // if (SentryClient.sharedClient && SentryClient.sharedClient.tags) {
+        //     id dict = [command.arguments objectAtIndex:0];
+        //     if ([dict isKindOfClass:NSDictionary.class]) {
+        //         NSMutableDictionary *newDict = [NSMutableDictionary new];
+        //         [newDict addEntriesFromDictionary:SentryClient.sharedClient.tags];
+        //         [newDict addEntriesFromDictionary:dict];
+        //         SentryClient.sharedClient.tags = newDict;
+        //     }
+        // }
     }];
 }
 
 - (void)setExtraContext:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        if (SentryClient.sharedClient && SentryClient.sharedClient.extra) {
-            id dict = [command.arguments objectAtIndex:0];
-            if ([dict isKindOfClass:NSDictionary.class]) {
-                NSMutableDictionary *newDict = [NSMutableDictionary new];
-                [newDict addEntriesFromDictionary:SentryClient.sharedClient.extra];
-                [newDict addEntriesFromDictionary:dict];
-                SentryClient.sharedClient.extra = newDict;
-            }
-        }
+        // if (SentryClient.sharedClient && SentryClient.sharedClient.extra) {
+        //     id dict = [command.arguments objectAtIndex:0];
+        //     if ([dict isKindOfClass:NSDictionary.class]) {
+        //         NSMutableDictionary *newDict = [NSMutableDictionary new];
+        //         [newDict addEntriesFromDictionary:SentryClient.sharedClient.extra];
+        //         [newDict addEntriesFromDictionary:dict];
+        //         SentryClient.sharedClient.extra = newDict;
+        //     }
+        // }
     }];
 }
 
 - (void)clearContext:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
-        [SentryClient.sharedClient clearContext];
+        // [SentryClient.sharedClient clearContext];
     }];
 }
 
---
+++ src/js/transports/cordova.ts
@@ -0,0 +1,68 @@
+import { FetchTransport, XHRTransport } from '@sentry/browser/dist/transports';
+import { Event, Response, Transport, TransportOptions } from '@sentry/types';
+import { logger, PromiseBuffer, SentryError, supportsFetch } from '@sentry/utils';
+
+import { NATIVE } from '../wrapper';
+
+/**
+ * Uses the native transport if available, otherwise falls back to Fetch/XHR transport.
+ * @see NATIVE.SUPPORTS_NATIVE_TRANSPORT for platforms with native transport
+ */
+export class CordovaTransport implements Transport {
+  /** A simple buffer holding all requests. */
+  protected readonly _buffer: PromiseBuffer<Response> = new PromiseBuffer(30);
+
+  /** Fallback transport uses if native transport is not available */
+  private _fallbackTransport: Transport;
+
+  constructor(options: TransportOptions) {
+    if (supportsFetch()) {
+      this._fallbackTransport = new FetchTransport(options);
+    } else {
+      this._fallbackTransport = new XHRTransport(options);
+    }
+
+    void this._checkNative();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  // @ts-ignore TODO: Need new JS version bump that uses PromiseLike instead of Promise otherwise this will error
+  public sendEvent(event: Event): PromiseLike<Response> {
+    // @ts-ignore TODO: For temporary debug purposes, DONT FORGET TO REMOVE!
+    event.tags['isNativeTransportAvailable'] = `${NATIVE.isNativeTransportAvailable()}`;
+
+    if (NATIVE.isNativeTransportAvailable()) {
+      if (!this._buffer.isReady()) {
+        return Promise.reject(new SentryError('Not adding Promise due to buffer limit reached.'));
+      }
+      return this._buffer.add(NATIVE.sendEvent(event));
+    }
+
+    return this._fallbackTransport.sendEvent(event);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  // @ts-ignore TODO: Need new JS version bump that uses PromiseLike instead of Promise otherwise this will error
+  public close(timeout?: number): PromiseLike<boolean> {
+    return Promise.all([this._buffer.drain(timeout), this._fallbackTransport.close()]).then(responses =>
+      responses.every(response => response)
+    );
+  }
+
+  /**
+   * Checks the platform to determine whether native transport is available.
+   */
+  private async _checkNative(): Promise<void> {
+    await NATIVE.getPlatform();
+
+    if (NATIVE.isNativeTransportAvailable()) {
+      logger.log(`Native Transport is available, using Native Transport.`);
+    } else {
+      logger.log(`Native Transport is not available, using Browser Transport.`);
+    }
+  }
+}
