40d6393dc6f6f19ab9b30a362b8cb763226e3a9f
jennmueng <ldrwarlock@gmail.com>
2021-03-19 17:01:16 +0700

    fix: Remove setRelease and setDist, have auto release passed to native
    As the Android native bridge has no support for setRelease and setDist which set __sentry_release and __sentry_dist as an extra, we remove these methods in favor of passing release and dist to init.
    We also have the SENTRY_RELEASE environment variable that is set at buildtime set the release on init instead.
    
    Closes #196

---
+++ src/js/__tests__/sdk.ts
@@ -0,0 +1,44 @@
+import { getGlobalObject } from '@sentry/utils';
+
+import { CordovaOptions } from '../backend';
+import * as Sdk from '../sdk';
+
+const optionsTest: {
+  current?: CordovaOptions;
+} = {
+  current: undefined,
+};
+
+jest.mock('@sentry/core', () => {
+  const core = jest.requireActual('@sentry/core');
+
+  return {
+    ...core,
+    initAndBind: jest.fn((_, options) => (optionsTest.current = options)),
+    makeMain: jest.fn(),
+  };
+});
+
+describe('Tests SDK', () => {
+  describe('init', () => {
+    it('Uses SENTRY_RELEASE environment variable if present.', () => {
+      const window = getGlobalObject<any>();
+      window.SENTRY_RELEASE = 'test-release';
+
+      Sdk.init({});
+
+      expect(optionsTest.current?.release).toBe('test-release');
+    });
+
+    it('User release has precedence over SENTRY_RELEASE', () => {
+      const window = getGlobalObject<any>();
+      window.SENTRY_RELEASE = 'test-release';
+
+      Sdk.init({
+        release: 'user-release',
+      });
+
+      expect(optionsTest.current?.release).toBe('user-release');
+    });
+  });
+});
