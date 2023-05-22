fd25d62ad338a297cc21791872db80f91a57d662
lucas <lucas-zimerman1@hotmail.com>
2023-05-22 21:45:28 +0900

    fix version bump

--- scripts/versionbump.js
+++ scripts/versionbump.js
@@ -6,23 +6,28 @@ if (!newVersion || !newVersion.match(/\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/)) {
   process.exit(1);
 }
 
+function ShowModifiedFiles(changedFiles) {
+  const files = changedFiles.map(item => item.file);
+  console.log('Modified files:', files.join(', '));
+}
+
 replace({
-  files: ['./src/ios/SentryCordova.m', './src/js/version.ts'],
+  files: ['./src/js/version.ts'],
   from: /\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/g,
   to: newVersion,
 })
   .then(changedFiles => {
-    console.log('Modified files:', changedFiles.join(', '));
+    ShowModifiedFiles(changedFiles);
     return replace({
       files: ['plugin.xml'],
       // from: /id="@sentry\/cordova" version="\d+\.\d+.\d+"/g,
       // to: `id="@sentry/cordova" version="${pjson.version}"`,
-      from: /id="sentry-cordova" version="\d+\.\d+.\d+"/g,
+      from: /id="sentry-cordova" version="\d+.\d+.\d+[^"]*"/g,
       to: `id="sentry-cordova" version="${newVersion}"`,
     });
   })
   .then(changedFiles => {
-    console.log('Modified files:', changedFiles.join(', '));
+    ShowModifiedFiles(changedFiles);
   })
   .catch(error => {
     console.error('Error occurred:', error);
